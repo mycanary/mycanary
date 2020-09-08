5c9767dfc13b05b331ffd21e79058f105ce8fa81
kus <kushaldeveloper@gmail.com>
2020-09-08 17:53:55 -0600

    add logger methods

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -6,11 +6,27 @@ import { Injectable } from '@angular/core';
 export class LoggerService {
 
   logs: Array<string> = [];
+  warnings: Array<string> = [];
+  errors: Array<string> = [];
 
   constructor() { }
 
   log(message: string) {
     this.logs.push(message);
     console.log(message);
+  }
+
+  warn(message: string) {
+    this.warnings.push(message);
+    console.warn(message);
+  }
+
+  error(message: string) {
+    this.errors.push(message);
+    console.error(message);
+  }
+
+  logObject(message: object) {
+    console.log({ message });
   }
 }
