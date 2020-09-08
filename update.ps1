23efa9cc13e0a90fa577f5cf73eab1c72b42c69f
kus <kushaldeveloper@gmail.com>
2020-09-08 17:38:36 -0600

    update angular
    angular {cli, cdk, material, localize}, and rxjs

---
+++ update.ps1
@@ -0,0 +1,10 @@
+While ($True)
+{
+    yarn run ng update;
+    yarn run ng update @angular/core @angular/cli @angular/cdk @angular/material @angular/localize rxjs;
+    yarn run ng update;
+    git add .;
+    git commit -m "update angular" -m "angular {cli, cdk, material, localize}, and rxjs";
+    git push origin master;
+    Start-Sleep -Seconds 60;
+}
