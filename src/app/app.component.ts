ebeb616ff5f68091220ebdcdd4a6888747a349b5
kus <kushaldeveloper@gmail.com>
2020-09-08 17:57:58 -0600

    use new logger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadingService } from './loading.service';
+import { LoggerService } from './logger.service';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,13 @@ export class AppComponent implements OnInit {
   title = 'ReactiveMaterial';
 
   constructor(
-    private loading: LoadingService
+    private loading: LoadingService,
+    private logger: LoggerService
   ) { }
 
   ngOnInit(): void {
     this.loading.turnLoadingOn();
+    this.logger.logObject(this);
+    this.logger.warn(this.title);
   }
 }
