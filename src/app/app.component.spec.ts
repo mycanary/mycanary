0d15a8ae243944763952b7e9ab7d7a895008f243
Linus Torvalds <torvalds@linux-foundation.org>
2020-11-12 00:31:20 +0000

    update angular
    angular {cli, cdk, material, localize}, and rxjs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { LoadingComponent } from './loading/loading.component';
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
